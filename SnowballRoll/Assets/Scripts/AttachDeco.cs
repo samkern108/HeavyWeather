abcf9003524ee23f2264cbda408da6c076d27b93
Sam Kern <samkern108@gmail.com>
2015-11-07 15:45:34 -0700

    More prefab stuff

---
+++ SnowballRoll/Assets/Scripts/AttachDeco.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttachDeco : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.transform.tag == "Player") {
+			transform.parent = other.transform;
+		}
+	}
+
+}
