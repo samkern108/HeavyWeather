06b0fe2b28b1f701bfd28cc706a2bfcaf15ed826
Sam Kern <samkern108@gmail.com>
2015-11-07 15:54:45 -0700

    Snowman beam

---
+++ SnowballRoll/Assets/Scripts/SnowmanBeam.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnowmanBeam : MonoBehaviour {
+
+	public GameObject headAttach;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.transform.tag == "Player") {
+			PlayerMovement.inst.NeearrrooommmSnowmanBeam(headAttach.transform);
+		}
+	}
+}
